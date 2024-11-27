35771d1dbd3b8e02fb3b916e7b2b89f88d881124
mohsin <pacificocean778@gmail.com>
2024-11-27 22:31:13 +0530

    Completed Hospital Management project

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("WELCOME TO HOTEL MANAGEMENT SYSTEM");
+        System.out.println("HOTEL MANAGEMENT SYSTEM");
 
         Scanner sc = new Scanner(System.in);
 
